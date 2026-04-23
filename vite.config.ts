99ed0e30404394476ec06b720f7fc9c29a603e3f
stha123surya-dotcom <stha123surya@gmail.com>
2026-04-23 15:19:31 +0545

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,18 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 import {defineConfig, loadEnv} from 'vite';
 
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/react-vite'
+
+export default defineConfig({
+  plugins: [react()],
+  base: 'Nekiwebsite', // Replace with your actual repo name
+}),
+
+
+
+  
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
